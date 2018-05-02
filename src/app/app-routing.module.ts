419d2a7a1e3ad0b73309d82a7edd5a768953791a
Chengyu Sun <csun@calstatela.edu>
2018-05-01 22:08:37 -0700

    Added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Block1Component } from './block1/block1.component';
+import { Block2Component } from './block2/block2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'block1', component: Block1Component },
+  { path: 'block2', component: Block2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { Block1Component } from './block1/block1.component';
+import { Block2Component } from './block2/block2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Block1Component,
+    Block2Component
   ],
   imports: [
     BrowserModule,
